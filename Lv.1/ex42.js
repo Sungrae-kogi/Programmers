39d7a1dc9efea4e43f6ecdf4fa886043706b5e95
Josh_corgi <whvkek@naver.com>
2024-04-05 23:44:10 +0900

    예산문제해결 자아성찰

--- Lv.1/ex42.js
+++ Lv.1/ex42.js
@@ -41,3 +41,24 @@ function solution(d, budget) {
     return max_count;
 }
 //bruteforce로 풀은 두번째풀이로, 이것역시 정확성검사에서 오답, 추후에 다시풀거나 해설을 찾아본다.
+//0405 다시 풀은 결과 -> 풀리지않아 검색을 해보았고 어이가없게도 처음 생각했던 대로 배열을 오름차순으로 정렬하고 밑에서부터
+//하나씩 뽑는게 최대의 결과를 내지않을까? 라는 생각이 옳았다고 한다.
+//이유를 찾아봤는데 너무나도 간단해서 허탈함이 크다, sort()함수를 그대로 사용했기때문으로, 자꾸 javascript의 정렬에는 
+//정렬함수를 전달해줘야한다는것을 까먹는일이 생기는거같다, 첫 코드에서 sort((a,b) => a-b)로 적어주면 마찬가지로 정답이 도출된다.
+//그냥 sort()를 하면 오답이 나오는 이유는 그냥 sort()함수는 2와 11을 비교했을때 문자순으로 정렬해버리기때문에 11과 2 로 정렬을 해버리기
+//때문에 오답을 도출하게되는거였다.
+function solution(d, budget) {
+    var answer = 0;
+    let count = 0;
+    let sorted_d = d.sort((a,b) => a-b);
+    for(let i=0; i<sorted_d.length; i++){
+        answer = answer + d[i];
+        count++;
+        if(answer > budget){
+            return count-1;
+        }else if(answer === budget){
+            return count;
+        }
+    }
+    return count;
+}
