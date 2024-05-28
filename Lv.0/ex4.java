415a08b15bd97772b2ba2cccf59295a0e8fb4fd1
Sungrae Cho <90044681+Sungrae-kogi@users.noreply.github.com>
2024-05-28 16:48:48 +0900

    Create ex4.java

---
+++ Lv.0/ex4.java
@@ -0,0 +1,24 @@
+//수 조작하기 1
+class Solution {
+    public int solution(int n, String control) {
+        
+        for(int i=0; i<control.length(); i++){
+            switch(control.charAt(i)){
+                case 'w':
+                    n++;
+                    break;
+                case 's':
+                    n--;
+                    break;
+                case 'd':
+                    n+=10;
+                    break;
+                case 'a':
+                    n-=10;
+                    break;
+            }
+        }
+        
+        return n;
+    }
+}
